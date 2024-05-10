ef28959acda4a869a9c1e2972f83f474620ea9ce
Angel Badillo Hernandez <badilloa022402@gmail.com>
2024-05-10 13:51:21 -0500

    added example static files mount

--- src/api.py
+++ src/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter, File, UploadFile, Query, Body, Path
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, RedirectResponse
 import uvicorn
 from routers import example
@@ -10,6 +11,8 @@ def docs_redirect():
     return RedirectResponse("/docs")
 
 app.include_router(example.router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
        uvicorn.run("api:app", port=8080, reload=True)
