2e48837444d4fe1616d3ac2ff61727b87fab7cdb
Angel Badillo Hernandez <81447537+It-Is-Legend27@users.noreply.github.com>
2024-06-25 21:35:51 -0500

    fix: upgraded postgres to latest version, fixed warning from using deprecated orm_mode in schemas, fixed warning for expected UUID, got str

--- app/schemas.py
+++ app/schemas.py
@@ -1,22 +1,6 @@
 from pydantic import BaseModel, Field, validate_email, field_validator
 from uuid import UUID, uuid4
 
-
-def uuid_validator(value: UUID) -> str:
-    """Validator function used for Pydantic BaseModels with UUID field.
-
-    Args:
-        value (UUID): A value of type UUID.
-
-    Returns:
-        str: string representation of UUID.
-    """
-    if value:
-        return str(value)
-    else:
-        return value
-
-
 class ItemBase(BaseModel):
     title: str
     description: str | None = None
@@ -30,11 +14,8 @@ class Item(ItemBase):
     id: UUID
     owner_id: UUID
 
-    _validate_id = field_validator("id")(uuid_validator)
-    _validate_owner_id = field_validator("owner_id")(uuid_validator)
-
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class UserBase(BaseModel):
@@ -50,12 +31,5 @@ class User(UserBase):
     is_active: bool
     items: list[Item] = []
 
-    _validate_id = field_validator("id")(uuid_validator)
-
     class Config:
-        orm_mode = True
-
-
-# class ExampleModel(BaseModel):
-#     name:str = Field(description="Name of thing")
-#     number:int = Field(description="Number of thing", ge=0)
+        from_attributes = True
